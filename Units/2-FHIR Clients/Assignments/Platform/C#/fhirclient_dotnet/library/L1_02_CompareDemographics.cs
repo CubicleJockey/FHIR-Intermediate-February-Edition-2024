fed4cdb5c2c9405fccec81e5307d9644a61dd5d8
André Davis <CubicleJockey@users.noreply.github.com>
2024-03-08 15:03:31 -0800

    Finished next section

--- Units/2-FHIR Clients/Assignments/Platform/C#/fhirclient_dotnet/library/L1_02_CompareDemographics.cs
+++ Units/2-FHIR Clients/Assignments/Platform/C#/fhirclient_dotnet/library/L1_02_CompareDemographics.cs
@@ -1,19 +1,47 @@
+using System.Linq;
+using System.Text;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+
 namespace fhirclient_dotnet
 {
     public class CompareDemographics
     {
-        public string GetDemographicComparison
-        (string ServerEndPoint,
-         string IdentifierSystem,
-         string IdentifierValue,
-         string myFamily,
-         string myGiven,
-         string myGender,
-         string myBirthDate
-         )
-         {
-            var aux="";
-            return aux;
-         }
+        public string GetDemographicComparison(string serverEndPoint, string identifierSystem, string identifierValue,
+            string family, string given, string gender, string birthDate)
+        {
+            var patient = new Patient();
+
+            using var client = new FhirClient(serverEndPoint);
+            var patientSearchBundle = client.Search<Patient>(new[] { $"identifier={identifierSystem}|{identifierValue}"});
+            if (patientSearchBundle.Entry.Count > 0)
+            {
+                patient = (Patient)patientSearchBundle.Entry[0].Resource;
+            }
+            else { return "Error:Patient_Not_Found"; }
+
+            var demographics = new StringBuilder();
+
+            var name = patient.Name.First();
+            var fhirFamily = name.Family;
+            var givenParts = name.Given.ToArray();
+
+            var fhirGiven = string.Join(" ", givenParts);
+            
+            var fhirGender = patient.Gender.ToString()?.ToUpper();
+            var fhirBirthDate = patient.BirthDate.ToString();
+
+            gender = gender.ToUpper();
+            
+            demographics.Append($"{{family}}|{family}|{fhirFamily}|{IsDifferent(family, fhirFamily)}\n");
+            demographics.Append($"{{given}}|{given}|{fhirGiven}|{IsDifferent(given, fhirGiven)}\n");
+            demographics.Append($"{{gender}}|{gender}|{fhirGender}|{IsDifferent(gender, fhirGender)}\n");
+            demographics.Append($"{{birthDate}}|{birthDate}|{fhirBirthDate}|{IsDifferent(birthDate, fhirBirthDate)}\n");
+            
+            
+            return demographics.ToString();
+        }
+
+        private static string IsDifferent(string local, string fhir) => local == fhir ? "{green}" : "{red}";
     }
 }
