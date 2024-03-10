bceb7a1be8e93fbfcde7a09ae7b31528e331f909
André Davis <CubicleJockey@users.noreply.github.com>
2024-03-10 00:36:35 -0800

    Finished FetchEthnicity

--- Units/2-FHIR Clients/Assignments/Platform/C#/fhirclient_dotnet/library/L2_01_FetchEthnicity.cs
+++ Units/2-FHIR Clients/Assignments/Platform/C#/fhirclient_dotnet/library/L2_01_FetchEthnicity.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using Hl7.Fhir.Model;
 using Hl7.Fhir.Rest;
 
@@ -13,19 +14,30 @@ namespace fhirclient_dotnet
             var patient = SearchPatient(serverEndPoint, identifierSystem, identifierValue);
             if (patient == default) { return "Error:Patient_Not_Found"; }
 
-            var (extensionFound, ethnicityExtension) = ExtensionContainsOmbCategory(patient.Extension);
+            var (extensionFound, ethnicityExtension) = ExtensionContainsUsCoreEthnicity(patient.Extension);
 
             if (patient.Extension.Count == 0 || !extensionFound)
             {
                 return "Error:No_us-core-ethnicity_Extension";
             }
 
-            var isConformant = CheckEthnicityExtensionsConformance(ethnicityExtension);
+            var (isConformant, code, text) = CheckConformanceAndRetrieveCodeAndText(ethnicityExtension);
             if (!isConformant) { return "Error:Non_Conformant_us-core-ethnicity_Extension"; }
+
+            var details = GetEthnicityDetails(ethnicityExtension).ToArray();
             
+            var response = new StringBuilder();
+            response.Append($"text|{text}\n");
+            response.Append($"code|{code}\n");
+            if (details.Any())
+            {
+                foreach (var detail in details)
+                {
+                    response.Append($"detail|{detail}\n");
+                }
+            }
             
-            var aux = "";
-            return aux;
+            return response.ToString();
 
         }
 
@@ -45,18 +57,49 @@ namespace fhirclient_dotnet
             return patient;
         }
 
-        private static (bool, Extension) ExtensionContainsOmbCategory(IEnumerable<Extension> extensions)
+        private static (bool found, Extension extension) ExtensionContainsUsCoreEthnicity(IEnumerable<Extension> extensions)
         {
-            var ethnicityExtension = extensions.FirstOrDefault(extension => extension.Url == "ombCategory");
-            var found = ethnicityExtension != default;
-            return (found, ethnicityExtension);
+            var invalid = (false, default(Extension));
+            
+            var ethnicityExtension = extensions.FirstOrDefault(extension => extension.Url == "http://hl7.org/fhir/us/core/StructureDefinition/us-core-ethnicity");
+            if (ethnicityExtension == default) { return invalid; }
+            
+            return (true, ethnicityExtension);
         }
 
-        private static bool CheckEthnicityExtensionsConformance(Extension extension)
+        /// <summary>
+        /// Must contain text and OMBCategory
+        /// </summary>
+        private static (bool isConformant, string code, string text) CheckConformanceAndRetrieveCodeAndText(IExtendable ethnicityExtension)
         {
-            //TODO: review documentation and complete logic.
+            var invalid = (false, default(string), default(string));
+            
+            var ombCategoryExtension = ethnicityExtension.Extension.FirstOrDefault(extension => extension.Url == "ombCategory");
+            if (ombCategoryExtension == default) { return invalid; }
 
-            return false;
+            var text = ethnicityExtension.Extension.FirstOrDefault(extension => extension.Url == "text");
+            if(text == default) { return invalid; }
+
+            var coding = (Coding)ombCategoryExtension.Value;
+            var fhirString = (FhirString)text.Value;
+
+            var code = $"{coding.Code}:{coding.Display}";
+            
+            return (true, code, fhirString.Value);
+        }
+
+        private static IEnumerable<string> GetEthnicityDetails(IExtendable ethnicityExtension)
+        {
+            var ethnicityDetailCodes = new List<string>();
+            foreach (var extension in ethnicityExtension.Extension)
+            {
+                if (extension.Url != "detailed") { continue; }
+
+                var coding = (Coding)extension.Value;
+                ethnicityDetailCodes.Add($"{coding.Code}:{coding.Display}");
+            }
+
+            return ethnicityDetailCodes;
         }
     }
 }
