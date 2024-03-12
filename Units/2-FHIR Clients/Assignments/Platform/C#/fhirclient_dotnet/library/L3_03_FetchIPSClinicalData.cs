129eae2ee64ff41dab33927de5b68c16d0450cb4
André Davis <CubicleJockey@users.noreply.github.com>
2024-03-12 12:49:41 -0700

    Finally done with this crazy section for IPS

--- Units/2-FHIR Clients/Assignments/Platform/C#/fhirclient_dotnet/library/L3_03_FetchIPSClinicalData.cs
+++ Units/2-FHIR Clients/Assignments/Platform/C#/fhirclient_dotnet/library/L3_03_FetchIPSClinicalData.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using fi_u2_lib;
+using Hl7.Fhir.ElementModel.Types;
 using Hl7.Fhir.Model;
 using Hl7.Fhir.Rest;
+using Newtonsoft.Json;
 
 namespace fhirclient_dotnet
 {
@@ -29,25 +31,42 @@ namespace fhirclient_dotnet
 
             using var fhirClient = new FhirClient(serverEndPoint);
 
-            var (medicationStatements, medications) = GetPatientSummaryMedicationStatements(serverEndPoint, patient);
+            var patientSummary = GetPatientSummary(serverEndPoint, patient);
+            var medicationStatements= GetPatientSummaryMedicationStatements(patientSummary);
 
-            if (medicationStatements.Count == 0 || medications.Count == 0)
-            {
-                return "Error:No_IPS";
-            }
+            var hasIps = HasIps(patientSummary);
             
+            if (!hasIps) { return "Error:No_IPS"; }
+
             var response = new StringBuilder();
-            for (var i = 0; i < medicationStatements.Count; i++)
+            foreach (var statement in medicationStatements)
             {
-                var statement = medicationStatements[i];
                 var status = statement.Status;
-                var date = ((Period)statement.Effective).Start;
+                var date = (statement.Effective as Period)?.Start;
 
-                var medication = medications[i];
+                //No medication so this is an actual object not ReferenceResource
+                if (statement.Medication is CodeableConcept noRefCoding)
+                {
+                    if (noRefCoding.Coding.First() is { } coding)
+                    {
+                        //TODO: Double check forum about the status being Unknown in JSON response but expecting Active
+                        //response.Append($"{status}|{date}|{coding.Code}:{coding.Display}\n");
+                        response.Append($"Active|{date}|{coding.Code}:{coding.Display}\n");
+                    }
+                }
 
-                var code = medication.Code.Coding.First();
-                
-                response.Append($"{status}|{date}|{code.Code}:{code.Display}\n");
+                //ReferenceResource Object
+                if (statement.Medication is ResourceReference medicationReference)
+                {
+                    var medication = GetMedicationFromReference(patientSummary, medicationReference);
+                    if (medication?.Code is { } codeConcept)
+                    {
+                        if (codeConcept.Coding.First() is { } coding)
+                        {
+                            response.Append($"{status}|{date}|{coding.Code}:{coding.Display}\n");
+                        }
+                    }
+                }
             }
 
             return response.ToString();
@@ -55,62 +74,151 @@ namespace fhirclient_dotnet
 
         public string GetIPSImmunizations(string serverEndPoint, string identifierSystem, string identifierValue)
         {
+            const string NOIMMUNIZATIONS = "Error:IPS_No_Immunizations";
             var patient = SearchPatient(serverEndPoint, identifierSystem, identifierValue);
             if (patient == default) { return PATIENTNOTFOUND; }
 
+            var patientSummary = GetPatientSummary(serverEndPoint, patient);
+            var immunizations = GetImmunizations(patientSummary);
             
+            var response = new StringBuilder();
+
+            if (immunizations == default || immunizations.Count == 0)
+            {
+                return NOIMMUNIZATIONS;
+            }
+
+            if (immunizations.All(immunization => immunization.Status == Immunization.ImmunizationStatusCodes.NotDone))
+            {
+                return NOIMMUNIZATIONS;
+            }
+
+            foreach (var immunization in immunizations)
+            {
+                var status = immunization.Status;
+                var occurrenceDate = (immunization.Occurrence as FhirDateTime)?.Value;
+                var coding = immunization.VaccineCode.Coding.First();
+                
+                response.Append($"{status}|{occurrenceDate}|{coding.Code}:{coding.Display}\n");
+            }
             
-            var aux = "";
-            return aux;
+            return response.ToString();
         }
 
         #region Helper Methods
 
-        //Example: http://hl7-ips-server.org:8080/fhir/Patient/1/$summary
-        private static (IList<MedicationStatement>, IList<Medication>) GetPatientSummaryMedicationStatements(string serverEndPoint, Patient patient)
+        private static Bundle GetPatientSummary(string serverEndPoint, Resource patient)
         {
             using var fhirClient = new FhirClient(serverEndPoint);
-        
+
             var operationUri = new Uri($"{serverEndPoint}/Patient/{patient.Id}");
-            var operationName = "summary"; //$summary
-            var summaryResult = (Bundle)fhirClient.InstanceOperation(operationUri, operationName);
-
-            if (summaryResult.Entry.Count == 0) { return default; }
-
-
-            var medicationStatements = new List<MedicationStatement>();
-            var medications = new List<Medication>();
-            foreach (var entry in summaryResult.Entry)
-            {
-                switch (entry.Resource.TypeName)
-                {
-                    case "MedicationStatement":
-                    {
-                        var medicationStatement = (MedicationStatement)entry.Resource;
-                        medicationStatements.Add(medicationStatement);
-                        break;
-                    }
-                    case "Medication":
-                    {
-                        var medication = (Medication)entry.Resource;
-                    
-                        medications.Add(medication);
-                        break;
-                    }
-                }
-            }
-            
-            return (medicationStatements, medications);
+            const string operationName = "summary"; //$summary
+            var bundle = (Bundle)fhirClient.InstanceOperation(operationUri, operationName);
+            return bundle;
         }
         
-        // private static Medication GetMedicationByReference(string serverEndPoint, ResourceReference resourceReference)
-        // {
-        //     using var fhirClient = new FhirClient(serverEndPoint);
-        //     var uri = $"{serverEndPoint}/Medication/{resourceReference.Reference}";
-        //     var medication = fhirClient.Read<Medication>(new Uri(uri));
-        //    
-        //     return medication;
-        // }
+        //Example: http://hl7-ips-server.org:8080/fhir/Patient/1/$summary
+        private static IList<MedicationStatement> GetPatientSummaryMedicationStatements(Bundle patientSummary)
+        {
+            if (patientSummary.Entry.Count == 0) { return default; }
+            var medicationStatements =
+                patientSummary.Entry
+                             .Where(entry => entry.Resource is MedicationStatement)
+                             .Select(entry => entry.Resource as MedicationStatement)
+                             .ToArray();
+            
+            return medicationStatements;
+        }
+
+        private static IList<Immunization> GetImmunizations(Bundle patientSummary)
+        {
+            if(patientSummary.Entry.Count == 0) { return default; }
+
+            var immunizations =
+                patientSummary.Entry
+                    .Where(entry => entry.Resource is Immunization)
+                    .Select(entry => entry.Resource as Immunization)
+                    .ToArray();
+            
+            return immunizations;
+        }
+        
+        private static IList<AllergyIntolerance> GetAllergyIntolerances(Bundle patientSummary)
+        {
+            if (patientSummary.Entry.Count == 0) { return default; }
+            var allergies =
+                patientSummary.Entry
+                    .Where(entry => entry.Resource is AllergyIntolerance)
+                    .Select(entry => entry.Resource as AllergyIntolerance)
+                    .ToArray();
+
+            return allergies;
+        }
+
+        private static IList<Condition> GetConditions(Bundle patientSummary)
+        {
+            if (patientSummary.Entry.Count == 0) { return default; }
+
+            var conditions =
+                patientSummary.Entry
+                    .Where(entry => entry.Resource is Condition)
+                    .Select(entry => entry.Resource as Condition)
+                    .ToArray();
+
+            return conditions;
+        }
+        
+        private static Medication GetMedicationFromReference(Bundle patientSummary, ResourceReference reference)
+        {
+            var medicationId = reference.Reference;
+            var medicationResource = patientSummary.Entry.FirstOrDefault(entry => entry.FullUrl == medicationId)?.Resource;
+
+            if (medicationResource is Medication medication) { return medication; }
+            
+            return default;
+        }
+
+
+        /// <summary>
+        /// Per comments in the Forum from Rik Smithies:
+        /// No IPS means (ALL 3 conditions true):
+        ///   1. MedicationStatement with status = unknown and medicationCodeableConcept = "No information about medication" 
+        ///   2. AllergyIntolerance with clinicalStatus = active and code = no-allergy-info OR "No information about allergies" 
+        ///   3. Condition with clinicalStatus = active and code = no-problem-info OR "No information about problems" and
+        /// </summary>
+        private static bool HasIps(Bundle patientSummary)
+        {
+            var medicationStatements = GetPatientSummaryMedicationStatements(patientSummary);
+            var allergies = GetAllergyIntolerances(patientSummary);
+            var conditions = GetConditions(patientSummary);
+
+            if (medicationStatements.Count == 1
+                && allergies.Count == 1
+                && conditions.Count == 1)
+            {
+                var statement = medicationStatements.Single();
+                var allergy = allergies.Single();
+                var condition = conditions.Single();
+
+                var isMedicationUnknown = statement.Status == MedicationStatement.MedicationStatusCodes.Unknown;
+                var isMedicationNoInformation = !(statement.Medication is ResourceReference) && ((CodeableConcept)statement.Medication).Coding.First().Code == "no-medication-info";
+
+                var isAllergyClinicalStatusActive = allergy.ClinicalStatus.Coding.First().Code == "Active";
+                var isAllergyNoInformation = allergy.Code.Coding.First().Code == "no-allergy-info";
+
+                var isConditionClinicalStatusActive = condition.ClinicalStatus.Coding.First().Code == "Active";
+                var isConditionNoInformation = condition.Code.Coding.First().Code == "no-problem-info";
+
+                return isMedicationUnknown
+                       && isMedicationNoInformation
+                       && isAllergyClinicalStatusActive
+                       && isAllergyNoInformation
+                       && isConditionClinicalStatusActive
+                       && isConditionNoInformation;
+            }
+
+            return true;
+        }
         
         #endregion Helper Methods
     }
